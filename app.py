c107d01aadf231fea620f08b35bf0b17f9f93f88
marcos <marcos.noriyuki.miyata@gmail.com>
2024-08-31 13:17:16 -0300

    Configurando corretamente o banco

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:password@localhost:5432/dbClubeEsportivo'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+DATABASE_URL = 'postgresql+psycopg2://postgres:password@localhost:5432/dbClubeEsportivo'
 
-db = SQLAlchemy(app)
+engine = create_engine(DATABASE_URL)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+app.config['SECRET_KEY'] = 'secret_key'
 
 # Não tirar essa importação ou o Flask não vai conseguir encontrar as rotas
 from routes import routes
